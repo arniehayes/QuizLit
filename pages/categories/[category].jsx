c05d8663f063d41d6fe445c02bef56276afec367
Arnie Hayes <arniehayes1@gmail.com>
2022-07-17 16:58:16 -0700

    testing

--- pages/categories/[category].jsx
+++ pages/categories/[category].jsx
@@ -15,7 +15,7 @@ export default [category];
 
 export const getStaticProps = async ({ params }) => {
   const results = await fetch(
-    `https://the-trivia-api.com/api/questions?categories=${category}&limit=20`
+    `https://the-trivia-api.com/api/questions?categories=${params.category}&limit=20`
   ).then((res) => res.json());
   return {
     props: {
@@ -26,14 +26,14 @@ export const getStaticProps = async ({ params }) => {
 
 export const getStaticPaths = async () => {
     const response = await fetch(
-        `https://the-trivia-api.com/api/questions?categories=${category}&limit=20`
+      `https://the-trivia-api.com/api/categories`
     ).then((res) => res.json());
-    const paths = response.map(data => {
+    const paths = response.map(category => {
         return {
-            params: {
-                data
-            }
-        }
+          params: {
+            category,
+          },
+        };
     });
     console.log(paths);
     return {
