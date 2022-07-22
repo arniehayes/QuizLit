bac2f002e21b5c0800fef8fdc43eba04ac2a57ef
Arnie Hayes <arniehayes1@gmail.com>
2022-07-21 23:18:09 -0700

    finished svg's

--- pages/categories/index.jsx
+++ pages/categories/index.jsx
@@ -11,15 +11,51 @@ const Category = () => {
       link: "arts_and_literature",
       path: "/painting-art-svgrepo-com.svg",
     },
-    { label: "Film & TV", link: "film_&_tv" },
-    { label: "Food & Drink", link: "food_and_drink" },
-    { label: "General Knowledge", link: "general_knowledge" },
-    { label: "Geography", link: "geography" },
-    { label: "History", link: "history" },
-    { label: "Music", link: "music" },
-    { label: "Science", link: "science" },
-    { label: "Society & Culture", link: "society_and_culture" },
-    { label: "Sport & Leisure", link: "sport_and_leisure" },
+    {
+      label: "Film & TV",
+      link: "film_&_tv",
+      path: "/film-strip-svgrepo-com.svg",
+    },
+    {
+      label: "Food & Drink",
+      link: "food_and_drink",
+      path: "/fast-food-svgrepo-com.svg",
+    },
+    {
+      label: "General Knowledge",
+      link: "general_knowledge",
+      path: "/book-svgrepo-com.svg",
+    },
+    {
+      label: "Geography",
+      link: "geography",
+      path: "/americasglobe-svgrepo-com.svg",
+    },
+    {
+      label: "History",
+      link: "history",
+      path: "/history-svgrepo-com.svg",
+    },
+    {
+      label: "Music",
+      link: "music",
+      path: "/music-svgrepo-com.svg",
+    },
+    {
+      label: "Science",
+      link: "science",
+      path: "/science-svgrepo-com.svg",
+    },
+    {
+      label: "Society & Culture",
+      link: "society_and_culture",
+      path: "/group-svgrepo-com.svg",
+    },
+    {
+      label: "Sport & Leisure",
+      link: "sport_and_leisure",
+      path: "/tennis-svgrepo-com.svg",
+    },
   ];
 
   return (
@@ -32,7 +68,7 @@ const Category = () => {
           {categoryList.map((category) => (
             <Link key={category.link} href={`/categories/${category.link}`}>
               <a className={style.anchor} id={category.link}>
-                <Image src={category.path} width={50} height={50} />
+                <Image src={category.path} width={60} height={60} />
                 {category.label}
               </a>
             </Link>
