017f104c7de7e296189158cd8f8df7c791207810
Arnie Hayes <arniehayes1@gmail.com>
2022-07-25 21:56:12 -0700

    webkit focus

--- pages/categories/[category].jsx
+++ pages/categories/[category].jsx
@@ -76,6 +76,7 @@ const Category = ({ results }) => {
             onClick={() => {
               setSubmit(true);
             }}
+            tabIndex="0"
           >
             Submit
           </button>
