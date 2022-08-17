4766cf308e25761b9ca8ef432e33f7027b1abad2
Arnie Hayes <arniehayes1@gmail.com>
2022-08-16 22:23:37 -0700

    api call fix

--- pages/categories/[category].jsx
+++ pages/categories/[category].jsx
@@ -41,7 +41,7 @@ const Category = ({ results }) => {
       setQuestionArray(newArr);
       console.log("questionArray after new call: ", questionArray);
     }
-    if (currentQuestion === 9) {
+    if (currentQuestion > 9) {
       fetchData();
     }
   },[currentQuestion])
